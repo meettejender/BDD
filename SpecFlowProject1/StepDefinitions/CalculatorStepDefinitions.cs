3b259673bac1710afbc5390db0d3ca2d508532c0
meettejender <meettejender@live.com>
2024-05-14 00:17:36 +0530

    Add project files.

---
+++ SpecFlowProject1/StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,44 @@
+using SpecFlowCalculator;
+
+
+namespace SpecFlowProject1.StepDefinitions
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        private readonly Calculator _calculator = new Calculator();
+        private readonly ScenarioContext _scenarioContext;
+        private int _result;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            _calculator.FirstNumber = number;
+
+
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            _calculator.SecondNumber = number;
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            _result = _calculator.Add();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            _result.Should().Be(result);
+        }
+    }
+}
